8b9f915081abd76f5e36b6f9d3b21c9ad001c1f5
charliee97 <carlosgcereceda@hotmail.es>
2018-05-03 20:01:04 +0200

    Comienzo mutacionFuncionalSimple

--- src/mutacion/MutacionTerminalSimple.java
+++ src/mutacion/MutacionTerminalSimple.java
@@ -8,6 +8,14 @@ import genotipo.GenotipoArbol;
 public class MutacionTerminalSimple<FenotipoUPB extends Fenotipo, FitnessUPB extends Fitness>
 implements Mutacion<GenotipoArbol, FenotipoArbol, FitnessUPB> {
 
+	/**
+	 *  Se selecciona al azar un símbolo
+		terminal dentro del individuo, y se sustituye por otro diferente
+		del conjunto de símbolos terminales posibles
+	 */
+	
+	
+	
 	@Override
 	public void muta(GenotipoArbol genotipo, FenotipoArbol fenotipo, double prob_mutacion) {
 		// TODO Auto-generated method stub
