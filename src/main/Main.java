fcdd63888f7d0fc474214e7f710c5eddde58b144
Rafael Andrés Hernández Roldán <rafaeher@ucm.es>
2018-11-02 01:54:52 +0100

    Commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import org.math.plot.utils.Array;
+
 import presentacion.Vista;
 
 public class Main
@@ -7,7 +9,14 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		Vista.getVista();
+
+		String[] patata = new String[5];
+		patata[0] = "P";
+		patata[1] = "A";
+		patata[2] = "t";
+		patata[3] = "a";
+		patata[4] = "ta";
+		System.out.println(Array.toString(patata));
 	}
 
 	
